e8b5e3fea857894e980df50bc2e96812438ba1da
Chelsey <cswilik@gmail.com>
2020-10-12 17:02:19 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,22 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
-end
+  winner = ""
+passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end 
+  end 
+  winner
+end 
+
+# def select_winner(passengers)
+#   winner = ""
+# passengers.each do |suite, name|
+#   if suite == :suite_a && name.start_with?("A")
+#     winner = name
+#   end
+# end
+ 
+# winner
+# end 
